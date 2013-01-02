93fd42bf283b404d821ccf5099cb3ec783acd85f
norayr <nornix@rambler.ru>
2013-01-02 23:15:01 +0300

    Добавлен филтр на валидость в Мodel_Setting

--- application/classes/model/setting.php
+++ application/classes/model/setting.php
@@ -2,4 +2,25 @@
 
 class Model_Setting extends ORM {
 protected $_primary_key = 'setting_id';
+
+	public function rules()
+    {
+        return array(
+            'url' => array(
+                array('not_empty'),
+            ),
+            'title' => array(
+                array('not_empty'),
+            ),
+			'email' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 4)),
+				array('max_length', array(':value', 127)),
+				array('email'),
+			),
+        );
+
+
+    }
+
 }
