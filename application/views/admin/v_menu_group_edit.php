026a8cbe33b969c0ffe4ee314fef6a879b2fee2b
svintusua <32760858+svintusua@users.noreply.github.com>
2018-10-27 01:25:52 +0300

    Update v_menu_group_edit.php
    Spelling

--- application/views/admin/v_menu_group_edit.php
+++ application/views/admin/v_menu_group_edit.php
@@ -1,7 +1,7 @@
 <div style="padding:10px;" class="border">
 	<form method="post" action=""><br>
 	<label>Название <input type="text" name="menu_group_title" /></label><br>
-	<label>Пазиция <input type="text" name="menu_group_position" /></label><br>
-	<input type="submit" name="submit" value="  Сахранить " />
+	<label>Позиция <input type="text" name="menu_group_position" /></label><br>
+	<input type="submit" name="submit" value="  Сохранить " />
 	</form>
 </div>
